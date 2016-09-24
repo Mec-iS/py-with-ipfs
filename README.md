3da313ba845e1d73959f05056f7f4980a2de3319
Lorenzo <tunedconsulting@gmail.com>
2016-09-24 11:53:58 +0200

    Initi repo. Ler's go.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # py-with-ipfs
+
 Python 3.5+ implementation for IPFS
+
+(more docs coming)
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='py-with-ipfs',
+    version='0.0.1dev',
+    description='Python Distribution Utilities',
+    author='py-with-ipfs Developers',
+    author_email='py-with-ipfs-developers@googlegroups.com',
+    url='https://github.com/Mec-iS/py-with-ipfs',
+    packages=[],
+)
