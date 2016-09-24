558c3168a0e10ee0a4e6a10f329f9ec8ab494dae
Lorenzo <tunedconsulting@gmail.com>
2016-09-24 12:10:48 +0200

    Add python3-ipfs-api for reference

--- setup.py
+++ setup.py
@@ -5,9 +5,12 @@ from distutils.core import setup
 setup(
     name='py-with-ipfs',
     version='0.0.1dev',
-    description='Python Distribution Utilities',
+    description='A Python implementation for IPFS',
     author='py-with-ipfs Developers',
     author_email='py-with-ipfs-developers@googlegroups.com',
     url='https://github.com/Mec-iS/py-with-ipfs',
     packages=[],
 )
+
+# python3-ipfs-api has to be cloned inside project directory:
+# git clone https://github.com/jgraef/python3-ipfs-api.git
